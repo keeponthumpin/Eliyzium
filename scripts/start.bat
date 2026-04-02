9472245c57fdf5096909228a57fd3a6030910a8c
Alex <keeponthumpin@gmail.com>
2026-04-02 11:23:46 +0200

    inital commit p2

---
+++ scripts/start.bat
@@ -0,0 +1,27 @@
+@echo off
+echo Current directory: %CD%
+cd /d "C:/Users/8DOF/Documents/8DOF_GUI/data/madmom"
+echo Changed directory to: %CD%
+set "PIP_DISABLE_PIP_VERSION_CHECK=1"
+if not exist venv_madmonTD (
+    echo Creating Python venv at: C:/Users/8DOF/Documents/8DOF_GUI/data/madmom\venv_madmonTD
+    "None" -m venv venv_madmonTD
+) else (
+    echo Virtual environment already exists at: C:/Users/8DOF/Documents/8DOF_GUI/data/madmom\venv_madmonTD
+)
+
+echo Attempting to activate virtual environment...
+call "venv_madmonTD\\Scripts\\activate.bat"
+
+rem Check if the virtual environment was activated successfully
+if "%VIRTUAL_ENV%" == "" (
+    echo Failed to activate virtual environment. Please check the path and ensure the venv exists.
+    echo Path to venv: "C:/Users/8DOF/Documents/8DOF_GUI/data/madmom\\venv"
+    echo VIRTUAL_ENV: "%VIRTUAL_ENV%"
+    pause /b 1
+) else (
+    echo Virtual environment activated.
+)
+echo Starting DNB BeatDetection
+python.exe "C:/Users/catsv/Desktop/Elyzium Project/scripts/DBNBeatTrackerOSC.py" --osc_serverip 127.0.0.1 --osc_serverport 7088 online --device 0
+pause
